a15b3464d30381bc139e12b417fa32d5f81b9855
Carlos Gustavo Lopez Pombo <clpombo@dc.uba.ar>
2024-04-03 17:09:09 -0300

    Minor changes.

--- src/data_channel_defs.h
+++ src/data_channel_defs.h
@@ -4,9 +4,9 @@
 #define BUFFER_SIZE 65536 // 64K(os default) max string length to call (from the code) a test action (to be executed by the simulator)
 //  [ INSTRUMENTACION: Define el tamano del evento más largo reportado. ]
 //  [ IMPORTANTE: 0 < __MAX_EVENT_SIZE <= 65528 ]
-#define MAX_EVENT_SIZE 310 // 308 is the longest event string plus 2 for the '\n'.
+#define MAX_EVENT_SIZE (308 + 2) // 308 is the longest event string plus 2 for the '\n'.
 //
-#define MAX_EVENT_PKG_SIZE (MAX_EVENT_SIZE + 8)
+#define MAX_EVENT_PKG_SIZE (MAX_EVENT_SIZE + 12)
 #define BUFFER_CAPACITY (BUFFER_SIZE / MAX_EVENT_PKG_SIZE)
 
 //Structure of component_event packages
