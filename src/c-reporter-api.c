257fe8cfad51084cc5ce03703b27a0f096cf00c0
Carlos Gustavo Lopez Pombo <clpombo@dc.uba.ar>
2024-04-03 16:32:10 -0300

    Minor changes.

--- src/c-reporter-api.c
+++ src/c-reporter-api.c
@@ -24,11 +24,11 @@ void report (clock_t time, eventType event_type, char* event){
     pkg.event_type = event_type;
     switch (pkg.event_type){
         case component_event: {
-            sprintf(pkg.event.component_event_pkg.data, "%-*s\n", MAX_EVENT_SIZE, event);
+            sprintf(pkg.event.component_event_pkg.data, "%-*s\n", MAX_EVENT_SIZE-2, event);
             break;
         }
         case workflow_event:{
-            sprintf(pkg.event.workflow_event_pkg.data, "%-*s\n", MAX_EVENT_SIZE, event);
+            sprintf(pkg.event.workflow_event_pkg.data, "%-*s\n", MAX_EVENT_SIZE-2, event);
             break;
         }
         default:
