8b4b588473e305c1e4507d7da6b02654a71f562a
Carlos Gustavo Lopez Pombo <clpombo@dc.uba.ar>
2024-04-03 16:08:54 -0300

    Minor changes.

--- src/data_channel_defs.h
+++ src/data_channel_defs.h
@@ -2,21 +2,21 @@
 #define __DATA_CHANNEL_DEFS_H
 
 #define BUFFER_SIZE 65536 // 64K(os default) max string length to call (from the code) a test action (to be executed by the simulator)
-#ifndef MAX_EVENT_SIZE
-    #define MAX_EVENT_SIZE 119
-#endif
-
-#define MAX_EVENT_PKG_SIZE (MAX_EVENT_SIZE + 1 + 8)
+//  [ INSTRUMENTACION: Define el tamano del evento más largo reportado. ]
+//  [ IMPORTANTE: 0 < __MAX_EVENT_SIZE <= 65528 ]
+#define MAX_EVENT_SIZE 309 // 308 is the longest event string plus 1 for the '\0'.
+//
+#define MAX_EVENT_PKG_SIZE (MAX_EVENT_SIZE + 8)
 #define BUFFER_CAPACITY (BUFFER_SIZE / MAX_EVENT_PKG_SIZE)
 
 //Structure of component_event packages
 typedef struct {
-    char data[MAX_EVENT_SIZE + 1];
+    char data[MAX_EVENT_SIZE];
 } componentEventPkg;
 
 //Structure of workflow_event packages
 typedef struct {
-    char data[MAX_EVENT_SIZE + 1];
+    char data[MAX_EVENT_SIZE];
 } workflowEventPkg;
 
 //classification of the different types of events
